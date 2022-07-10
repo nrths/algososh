2f27b7aa1499c726c3c3e89f100b9cbec65b759f
nrths <northnosouth@gmail.com>
2022-07-10 03:24:41 +0300

    initial commit, added main components to the string-page

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { FormEvent, SyntheticEvent, useState } from "react";
+import styles from './string.module.css';
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
+  const [string, setString] = useState<string>('')
+
+  const handleChange = (evt: SyntheticEvent<HTMLInputElement>) => {
+    setString(evt.currentTarget.value)
+  }
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+  }
+
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input isLimitText={true} maxLength={11} onChange={handleChange}/>
+        <Button text='Развернуть' />
+      </form>
+      <div className={styles.algorithm}>
+        <Circle />
+      </div>
     </SolutionLayout>
   );
 };
